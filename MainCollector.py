4b66aa6874fb06789cd062f996532819677950dc
milknono <milknono@korea.ac.kr>
2025-08-02 13:54:31 +0900

    노현서 1.4

--- MainCollector.py
+++ MainCollector.py
@@ -4,12 +4,12 @@ import config
 import corpcode as cc ##corpcode 관련 함수들
 import reportfinder as rpf ###report finder 함수들
 
+######config######
+homePath = os.getcwd()
+DartFile_path = os.path.join(homePath, 'DartFile')
+###############
 
 def main():
-    homePath = os.getcwd()
-    DartFile_path = os.path.join(homePath, 'DartFile')
-
-
     ### Dart api key
     print(homePath) ### testcode
     if not os.path.isdir(DartFile_path):
@@ -17,21 +17,20 @@ def main():
 
 
     # 1. 전체 기업 코드 다운로드
-    cc.download_cc(homePath)
+    cc.download_cc()
 
 
     # 2. CORPCODE.xml에서 corp_code(기업코드) 찾기
     target_corp = input('기업명 입력:')
-    corp_code = cc.find_cc(homePath ,target_corp)
-
+    corp_code = cc.find_cc(target_corp)
 
     # 3. 재무제표 공시 검색 및 저장(사업, 반기, 분기보고서)
-    rpf.find_finance_report(homePath, corp_code, begin=input("begin(8자리로 입력):"), end=input("end(8자리로 입력):"))
-
+    BEGINDATE=input("보고서 검색 시작일(8자리로 입력):")
+    ENDDATE=input("보고서 검색 종료일(8자리로 입력):")
+    rpf.find_finance_report(corp_code, BEGINDATE, ENDDATE)
 
     # 4. 실적보고서 관련 공시 검색 및 저장
-    
-
+    ##rpf.find_other_report(corp_code, BEGINDATE, ENDDATE)
 
 
 if __name__ == "__main__":
--- corpcode.py
+++ corpcode.py
@@ -4,21 +4,22 @@ import pandas as pd
 import requests
 import zipfile
 import xml.etree.ElementTree as ET
-from config import API_KEY
-api_key = API_KEY
+import config
 
-def download_cc(homepath):
+api_key = config.API_KEY
+
+def download_cc():
     corp_code_url = f'https://opendart.fss.or.kr/api/corpCode.xml?crtfc_key={api_key}'
     with open('corpCode.zip', 'wb') as f:
         f.write(requests.get(corp_code_url).content)
     with zipfile.ZipFile('corpCode.zip', 'r') as zip_ref:
-        zip_ref.extractall(homepath)
+        zip_ref.extractall(config.homePath)
     if os.path.exists('corpCode.zip'):
         os.remove('corpCode.zip')
     print(f'기업코드 다운 완료')
 
-def find_cc(homepath, target_corp):
-    tree = ET.parse(os.path.join(homepath, 'CORPCODE.xml'))
+def find_cc(target_corp):
+    tree = ET.parse(os.path.join(config.homePath, 'CORPCODE.xml'))
     root = tree.getroot()
     corp_code = None
 
